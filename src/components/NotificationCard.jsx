5ed5fdee558a08f1f96c0611fc81ba1cdd4cefe3
Karl Mellinger <mellinka@oregonstate.edu>
2025-01-15 10:32:53 -0800

    Updated time format

--- src/components/NotificationCard.jsx
+++ src/components/NotificationCard.jsx
@@ -4,10 +4,11 @@ function NotificationCard({notification}) {
     // Convert timestamp to Day Month Year, Time
     function getTimestamp(timestamp) {
         const date = new Date(timestamp);
-        
-        return new Intl.DateTimeFormat("en-US", 
+        const intlFormattedDate = new Intl.DateTimeFormat("en-US", 
             { day: "2-digit", month: "short", year: "numeric", hour: "numeric", minute: "numeric", hour12: true }
         ).format(date).replace(",", "");
+        const [month, day, year, time, a] = intlFormattedDate.split(" ");
+        return `${ day } ${ month } ${ year } ${ time }${ a.toLowerCase() }`;
     }
     return (
         <div className={`notification-card ${notification.type}`}>
